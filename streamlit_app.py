c28e71c889253875da24cd98a66f03ede1cbfd2d
favas-vazhayil <141208579+favas-vazhayil@users.noreply.github.com>
2023-08-04 15:09:30 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,49 @@
+import pickle
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+page_bg_img = '''
+<style>
+[data-testid="stAppViewContainer"]{
+background-image: url("https://images.unsplash.com/photo-1598960087461-556c5a1f864a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80");
+background-size: cover;
+}
+[data-testid="stHeader"]{
+background-color: rgba(0,0,0,0);
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title('Book Recommendation System')
+pt=pickle.load(open('pivot.pkl','rb'))
+finalbooks=pickle.load(open('finalbooks.pkl','rb'))
+similarity_score = cosine_similarity(pt)
+def recommend(book_name):
+    # index fetch
+    index = np.where(pt.index==book_name)[0][0]
+    similar_items = sorted(list(enumerate(similarity_score[index])),key=lambda x:x[1],reverse=True)[1:7]
+    
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = finalbooks[finalbooks['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        
+        data.append(item)
+    
+    return data
+input_book = st.selectbox("Enter or select the name of book",pt.index)
+
+if st.button('Recommend similar Books'):
+    data_1=recommend(input_book)
+    for i in data_1:
+        st.subheader(i[0])
+        st.write('Author : ', i[1])
+        st.image(i[2])
+        st.write("**______________________________________________________________________**")
+    
