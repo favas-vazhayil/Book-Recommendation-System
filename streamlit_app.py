828f0b24849bcf7e4fd7cb0bed30ed69c6f454ff
favas-vazhayil <141208579+favas-vazhayil@users.noreply.github.com>
2023-08-05 09:46:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 page_bg_img = '''
 <style>
 [data-testid="stAppViewContainer"]{
-background-image: url("https://images.unsplash.com/photo-1553447977-754f9430685c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80");
+background-image: url("https://plus.unsplash.com/premium_photo-1677187301444-fd793e33e8d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1823&q=80");
 background-size: cover;
 }
 [data-testid="stHeader"]{
